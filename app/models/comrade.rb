04a7e106e08ec5f61aa130880a52a0207bb9f832
Gary Hall <guategary@gmail.com>
2017-12-26 10:13:48 -0500

    Set default for accepted to false

--- app/models/comrade.rb
+++ app/models/comrade.rb
@@ -2,15 +2,23 @@ class Comrade < ApplicationRecord
   belongs_to :requestor, class_name: 'User', foreign_key: 'requestor_id'
   belongs_to :requestee, class_name: 'User', foreign_key: 'requestee_id'
 
+  after_initialize :defaults
+
   validates :requestor_id, presence: true
   validates :requestee_id, presence: true
 
-  scope :request, -> { where(:accepted => nil) }
+  scope :request, -> { where(:accepted => false) }
   scope :accepted, -> { where(:accepted => true) }
 
   def self.from_profile(id1, id2)
     where(requestor_id: id1, requestee_id: id2).or(Comrade.where(requestor_id: id2, requestee_id: id1)).first
   end
+
+  private
+
+    def defaults
+      self.accepted = false if self.accepted.nil?
+    end
 
 end
 
